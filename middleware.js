3744802040a64247e4ae82affe51f49d397d0aa6
HariKrishna.Pamidala <HariKrishna.Pamidala@ucb.com>
2025-12-03 23:11:15 +0100

    new

--- middleware.js
+++
@@ -1,5 +0,0 @@
-// middleware.js
-export { default } from './src/middleware/rate-limiter';
-export const config = {
-  matcher: ['/api/:path*', '/dashboard/:path*', '/library/:path*', '/match/:path*'],
-};
